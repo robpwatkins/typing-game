df905948e63558c5ffe809224428ef9605ba9e75
robpwatkins <robpwatkins@gmail.com>
2021-01-23 21:03:29 -0600

    now THATS a good days work

--- src/components/TypingGame.js
+++ src/components/TypingGame.js
@@ -2,48 +2,61 @@ import React, { useState, useEffect, useCallback } from 'react';
 import { words } from '../words';
 
 export default function TypingGame() {
-  const [wordIndex, setWordIndex] = useState(0);
   const [letterIndex, setLetterIndex] = useState(0);
+  const [wordList, setWordList] = useState([]);
   
   const handleKeyUp = useCallback(event => {
-    let currentWord = words[wordIndex];
+    let currentWord = wordList[wordList.length - 1];
     let currentLetter = currentWord.charAt(letterIndex);
     if (event.key === currentLetter) {
       if (currentWord.length === letterIndex + 1) {
-        setWordIndex(wordIndex + 1);
-        setLetterIndex(0);
+        if (wordList.length === 1) {
+          setWordList(["You Did It!!!"]);
+        } else {
+          let tempWordList = wordList.slice();
+          tempWordList.pop();
+          setWordList(tempWordList);
+          setLetterIndex(0);
+        }
       } else
       setLetterIndex(letterIndex + 1);
     }
-  }, [letterIndex, wordIndex])
+  }, [letterIndex, wordList])
+  
+  useEffect(() => {
+    let tempWordArr = [];
+    for (let i = 0; i < 10; i++) {  
+      tempWordArr.unshift(words[i]);
+    }
+    setWordList(tempWordArr);
+  }, [])
   
   useEffect(() => {
     window.addEventListener('keyup', handleKeyUp);
     return () => window.removeEventListener('keyup', handleKeyUp)
   }, [handleKeyUp])
   
-  console.log(words[wordIndex].charAt(letterIndex));
   return (
     <section>
-      {words.map((word, index) => {
-        if (index > wordIndex) {
-          return (
-            <p key={index}>{word}</p>
-          )
-        } else if (index === wordIndex) {
-          return (
-            word.split('').map((letter, idx) => {
-              return (
-                <span 
-                  key={idx}
-                  style={idx < letterIndex ? {fontWeight: "bold"} : null}
-                >
-                  {letter}
-                </span>
-              )
-            })
-          )
-        }
+      {wordList.map((word, index) => {
+        return (
+          <p key={index}>{
+            index < wordList.length - 1
+            ? word
+            : (
+              wordList[wordList.length - 1].split('').map((letter, index) => {
+                return (
+                  <span
+                    key={index}
+                    style={index < letterIndex ? {fontWeight: "bold"} : null}
+                  >
+                    {letter}
+                  </span>
+                )
+              })
+            )
+          }</p>
+        )
       })}
     </section>
   )
---
+++ src/components/Word.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Word({ word }) {
+  return (
+    <p>{word}</p>
+  )
+}
