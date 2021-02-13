90a480bec3415d0c27ad4b033e65176a616c8a73
robpwatkins <robpwatkins@gmail.com>
2021-02-13 12:06:34 -0600

    now THAT could be it

--- src/components/FX.js
+++ src/components/FX.js
@@ -3,15 +3,15 @@ import { VolumeMute, VolumeOff } from '@material-ui/icons';
 
 export default function FX({ fxEnabled, handleClick }) {
   return (
-    <>
+    <div className="fx">
       {fxEnabled 
-        ? <div>
+        ? <div className="volume-mute">
             <VolumeMute style={{fontSize: "4vh"}} onClick={handleClick} />
           </div>
         : <div>
             <VolumeOff style={{fontSize: "4vh"}} onClick={handleClick} />
           </div>
       }
-    </>
+    </div>
   )
 }
