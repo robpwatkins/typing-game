733c090f3e0044994165907264a293a432af9261
robpwatkins <robpwatkins@gmail.com>
2021-02-14 11:32:40 -0600

    changed the volume icons to two of the same default size

--- src/components/FX.js
+++ src/components/FX.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { VolumeMute, VolumeOff } from '@material-ui/icons';
+import { VolumeUp, VolumeOff } from '@material-ui/icons';
 
 export default function FX({ fxEnabled, handleClick }) {
   return (
     <div className="fx">
       {fxEnabled 
-        ? <div className="volume-mute">
-            <VolumeMute style={{fontSize: "5vh"}} onClick={handleClick} />
-          </div>
-        : <div className="volume-off">
-            <VolumeOff style={{fontSize: "5vh"}} onClick={handleClick} />
-          </div>
+        ? <VolumeUp style={{fontSize: "4vh"}} onClick={handleClick} />
+        : <VolumeOff style={{fontSize: "4vh"}} onClick={handleClick} />
+      }
+      {
+        // ? <div className="volume-mute">
+            // <VolumeMute style={{fontSize: "5vh"}} onClick={handleClick} />
+        //   </div>
+        // : <div className="volume-off">
+            // <VolumeOff style={{fontSize: "5vh"}} onClick={handleClick} />
+        //   </div>
       }
     </div>
   )
