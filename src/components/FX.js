70295f330fb2c55c47c6ca92b869514069927dc8
robpwatkins <robpwatkins@gmail.com>
2021-02-14 11:37:12 -0600

    and got rid of divs for icons because they stay in the same place now

--- src/components/FX.js
+++ src/components/FX.js
@@ -8,14 +8,6 @@ export default function FX({ fxEnabled, handleClick }) {
         ? <VolumeUp style={{fontSize: "4vh"}} onClick={handleClick} />
         : <VolumeOff style={{fontSize: "4vh"}} onClick={handleClick} />
       }
-      {
-        // ? <div className="volume-mute">
-            // <VolumeMute style={{fontSize: "5vh"}} onClick={handleClick} />
-        //   </div>
-        // : <div className="volume-off">
-            // <VolumeOff style={{fontSize: "5vh"}} onClick={handleClick} />
-        //   </div>
-      }
     </div>
   )
 }
