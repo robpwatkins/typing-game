3049f99fe4df633a45d5c44f071da1827911d452
robpwatkins <53236017+robpwatkins@users.noreply.github.com>
2021-02-14 11:42:23 -0600

    Hosting on Netlify, using env variables there

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function StartScreen({ handleClick }) {
   return (
     <section className="start-screen">
-      <h3>Typing Game!</h3>
+      <h3>Typing Game</h3>
       <button onClick={handleClick}>START GAME</button>
     </section>
   )
