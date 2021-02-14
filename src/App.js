fa8c18ef224641f5ffa155cff4c97b5180e73feb
robpwatkins <robpwatkins@gmail.com>
2021-02-14 13:01:39 -0600

    got global styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import { useState } from 'react';
-import './App.css';
+import { createGlobalStyle } from 'styled-components';
+import styled from 'styled-components';
 import StartScreen from './components/StartScreen';
 import TypingGame from './components/TypingGame';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+const Div = styled.div`
+  width: 100%;
+  height: 100vh;
+  text-align: center;
+  background-color: ghostwhite;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: 4vh;
+  font-family: 'Courier New', Courier, monospace;
+  color: #282c34;
+`;
 
 function App() {
   const [gameStarted, setGameStarted] = useState(false);
   const [difficulty, setDifficulty] = useState(null);
 
   return (
-    <div className="App">
+    <Div className="App">
+      <GlobalStyle />
       {!gameStarted 
         ? <StartScreen
             setGameStarted={setGameStarted}
@@ -20,7 +45,7 @@ function App() {
             setGameStarted={setGameStarted}
             difficulty={difficulty} 
           />}
-    </div>
+    </Div>
   );
 }
 
