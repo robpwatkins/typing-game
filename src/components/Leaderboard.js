3b9d4561fdffab32eb325e3101ee34cf8f2372f4
robpwatkins <robpwatkins@gmail.com>
2021-05-01 11:09:39 -0500

    got leaderboard up there

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Div = styled.div`
+  position: fixed;
+  top: 10%;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  justify-content: center;
+`;
 
 export default function Leaderboard() {
   return (
-    <>
+    <Div>
       <h4>Leaderboard</h4>
       <h6>Player 1</h6>
       <h6>Player 2</h6>
       <h6>Player 3</h6>
       <h6>Player 4</h6>
       <h6>Player 5</h6>
-    </>
+    </Div>
   )
 }
