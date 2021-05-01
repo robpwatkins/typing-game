3e50951ae111857a8e284cfe5f2eee1030556d8c
robpwatkins <robpwatkins@gmail.com>
2021-05-01 17:03:24 -0500

    boom auth0 is connected

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN
-const clientid = process.env.REACT_APP_CLIENT_ID
+const clientid = process.env.REACT_APP_AUTH0_CLIENT_ID
 
 ReactDOM.render(
   <Auth0Provider
